2fad6527aaabbbfdb0f69fb07fd6fa5ee6c826fa
elehobica <elehobica@gmail.com>
2024-03-31 15:38:30 +0900

    use submodule for pico_st7735_80x160. toggle peak hold mode on any key

--- README.md
+++ README.md
@@ -33,9 +33,10 @@ The frontend analog circuit should be needed.
 
 ## How to build
 * See ["Getting started with Raspberry Pi Pico"](https://datasheets.raspberrypi.org/pico/getting-started-with-pico.pdf)
-* Put "pico-sdk", "pico-examples" (, "pico-extras" and "pico-playground") on the same level with this project folder.
+* Put "pico-sdk", "pico-examples" and "pico-extras" on the same level with this project folder.
+* Set environmental variables for PICO_SDK_PATH, PICO_EXTRAS_PATH and PICO_EXAMPLES_PATH
 * Build is confirmed in Developer Command Prompt for VS 2022 and Visual Studio Code on Windows enviroment
-* Confirmed with Pico SDK 1.4.0, cmake-3.27.2-windows-x86_64 and gcc-arm-none-eabi-10.3-2021.10-win32
+* Confirmed with Pico SDK 1.5.1, cmake-3.27.2-windows-x86_64 and gcc-arm-none-eabi-10.3-2021.10-win32
 ```
 > git clone -b master https://github.com/raspberrypi/pico-sdk.git
 > cd pico-sdk
@@ -44,6 +45,9 @@ The frontend analog circuit should be needed.
 > git clone -b master https://github.com/raspberrypi/pico-examples.git
 > 
 > git clone -b main https://github.com/elehobica/pico_level_meter.git
+> cd pico_level_meter
+> git submodule update -i
+> cd ..
 ```
 * Lanuch "Developer Command Prompt for VS 2022"
 ```
@@ -54,3 +58,6 @@ The frontend analog circuit should be needed.
 > nmake
 ```
 * Put "pico_level_meter.uf2" on RPI-RP2 drive
+
+## Peak Hold
+* push any key on serial terminal to toggle peak hold mode
--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,7 @@
 #include <algorithm>
 
 #include "pico/stdlib.h"
-#include "hardware/pwm.h"
-#include "st7735_80x160/my_lcd.h"
+#include "lcd_extra.h"
 
 // dbScale (max - min <= 40, otherwise lowest scale has no meaning against 12bit ADC resolution)
 //static const std::vector<float> dbScale{-20, -15, -10, -6, -4, -2, 0, 1, 2, 6, 8};  // default
@@ -20,7 +19,7 @@ static const int NUM_LEVELS = dbScale.size();
 static int greenTh;
 static int redTh;
 
-#define PEAK_HOLD
+static bool peakHoldFlag = true;
 
 void prepareLevel()
 {
@@ -37,9 +36,9 @@ void prepareLevel()
 void drawLevelMeter(int ch, int level, int peakHold = -1)
 {
     const u16 Y_CH_HEIGHT = 10;
-    const u16 Y_OFFSET = LCD_HEIGHT / 2 - Y_CH_HEIGHT;
-    const u16 WIDTH = LCD_WIDTH / NUM_LEVELS;
-    const u16 X_OFFSET = (LCD_WIDTH -  WIDTH * NUM_LEVELS) / 2;
+    const u16 Y_OFFSET = LCD_H() / 2 - Y_CH_HEIGHT;
+    const u16 WIDTH = LCD_W() / NUM_LEVELS;
+    const u16 X_OFFSET = (LCD_W() -  WIDTH * NUM_LEVELS) / 2;
 
     for (int i = 0; i < NUM_LEVELS; i++) {
         if (i == 0 || i < level || i == peakHold) {  // level0 is always on
@@ -55,43 +54,47 @@ int main()
 {
     stdio_init_all();
 
-    // BackLight PWM (125MHz / 65536 / 4 = 476.84 Hz)
-    gpio_set_function(PIN_LCD_BLK, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN);
-    pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config, 4.f);
-    pwm_init(slice_num, &config, true);
-    int bl_val = 196;
-    // Square bl_val to make brightness appear more linear
-    pwm_set_gpio_level(PIN_LCD_BLK, bl_val * bl_val);
+    pico_st7735_80x160_config_t lcd_cfg = {
+        SPI_CLK_FREQ_DEFAULT,
+        spi1,
+        PIN_LCD_SPI1_CS_WAVESHARE,
+        PIN_LCD_SPI1_SCK_WAVESHARE,
+        PIN_LCD_SPI1_MOSI_WAVESHARE,
+        PIN_LCD_DC_WAVESHARE,
+        PIN_LCD_RST_WAVESHARE,
+        PIN_LCD_BLK_WAVESHARE,
+        PWM_BLK_DEFAULT,
+        INVERSION_DEFAULT,  // 0: non-color-inversion, 1: color-inversion
+        RGB_ORDER_DEFAULT,  // 0: RGB, 1: BGR
+        ROTATION_DEFAULT,
+        H_OFS_DEFAULT,
+        V_OFS_DEFAULT,
+        X_MIRROR_DEFAULT
+    };
 
-    u8 rotation = 3;
+    LCD_Config(&lcd_cfg);
     LCD_Init();
-    LCD_SetRotation(rotation);
+    LCD_SetRotation(1);
     LCD_Clear(BLACK);
     BACK_COLOR=BLACK;
 
 
     // level meter
+    int level[NUM_ADC_CH];
+    int peakHold[NUM_ADC_CH];
     level_meter::init(dbScale);
     level_meter::start();
     prepareLevel();
 
     while (true) {
-        int level[NUM_ADC_CH];
-#if defined(PEAK_HOLD)
-        int peakHold[NUM_ADC_CH];
+        if (getchar_timeout_us(0) != PICO_ERROR_TIMEOUT) peakHoldFlag = !peakHoldFlag;  // any key to toggle peakHold
         if (level_meter::get_level(level, peakHold)) {
-#else //defined(PEAK_HOLD)
-        if (level_meter::get_level(level)) {
-#endif
-            printf("level %d %d\n", level[0], level[1]);
             for (int i = 0; i < NUM_ADC_CH; i++) {
-#if defined(PEAK_HOLD)
-                drawLevelMeter(i, level[i], peakHold[i]);
-#else //defined(PEAK_HOLD)
-                drawLevelMeter(i, level[i]);
-#endif
+                if (peakHoldFlag) {
+                    drawLevelMeter(i, level[i], peakHold[i]);
+                } else {
+                    drawLevelMeter(i, level[i]);
+                }
             }
         }
     }
